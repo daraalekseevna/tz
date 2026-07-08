1422eaa039c266b9303b74faa29c09a1166a94c9
daraalecseevna <darazadoroznaa2004@mail.ru>
2026-07-08 23:34:08 +0300

    23

--- test-gymteam/backend/config/email.js
+++ test-gymteam/backend/config/email.js
@@ -1,28 +1,26 @@
 // config/email.js
 const { Resend } = require('resend');
 
-// Инициализация Resend с вашим API ключом
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, htmlContent) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Амбассадор <onboarding@resend.dev>', // На время теста
+      from: 'onboarding@resend.dev',
       to: [to],
       subject: subject,
       html: htmlContent,
     });
     
     if (error) {
-      console.error('❌ Ошибка Resend:', error);
+      console.error('Ошибка Resend:', error);
       return false;
     }
     
-    console.log('✅ Письмо отправлено через Resend');
-    console.log('📨 ID:', data?.id);
+    console.log('Письмо отправлено через Resend, ID:', data?.id);
     return true;
   } catch (error) {
-    console.error('❌ Ошибка Resend:', error.message);
+    console.error('Ошибка Resend:', error.message);
     return false;
   }
 }
